9aff92896cfa4a70c76bdf407ff2a77f46702aeb
omar <omar.h2002@gmail.com>
2025-05-06 19:11:04 +0300

    change lab5 (3)

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/winner.java
@@ -0,0 +1,12 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+public class winner {
+    private final int winner;
+
+    public winner(int winner) {
+        this.winner = winner;
+    }
+    public int getWinner() {
+        return winner;
+    }
+}
